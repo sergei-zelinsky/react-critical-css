99cbe7c2303b4243962b51306f8644f231813764
Sergei Zelinsky <zelinsky@lun.ua>
2018-03-19 23:08:44 +0200

    withStyles: refactor a bit

--- src/withStyles/index.js
+++ src/withStyles/index.js
@@ -1,31 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const __SERVER__ = typeof window === 'undefined';
-
-const withStyles = (styles) => (WrappedComponent) => {
-  return class extends Component {
-
-    static contextTypes = {
-      registerStyles: PropTypes.func
-    };
-
-    componentWillMount = () => {
-      const {registerStyles} = this.context;
-
-      if (__SERVER__){
-        registerStyles && registerStyles(styles);
-      }
-    };
-
-    render(){
-      return (
-        <WrappedComponent
-          {...this.props}
-        />
-      );
-    }
-  }
-};
-
-export default withStyles;
+export default typeof window === 'undefined'
+  ? (
+      styles => (
+        WrappedComponent => (
+          class extends Component {
+            
+            static contextTypes = {
+              registerStyles: PropTypes.func
+            };
+        
+            componentWillMount = () => {
+              const {registerStyles} = this.context;
+        
+              if (registerStyles){
+                registerStyles(styles);
+              }
+            };
+        
+            render(){
+              return (
+                <WrappedComponent
+                  {...this.props}
+                />
+              );
+            }
+          }
+        )
+      )
+  )
+  : (() => _ => _);
